009a8890e1b5b6820c7540ae0977be67d2c8951c
TJ-Griffin <tjgriffin77@gmail.com>
2023-12-04 11:48:08 -0500

    dot following mouse working

--- screen.py
+++ screen.py
@@ -2,9 +2,13 @@ import pygame
 
 class Screen:
 	def __init__(self):
+		# pygame window initialization
 		pygame.init()
 		self.window = pygame.display.set_mode((600,800))
 		pygame.display.set_caption("Euler's Circuit")
+
+		# class variables
+		self.dots = []
 
 	def loop(self):
 		running = True
@@ -14,5 +18,15 @@ class Screen:
 				if event.type == pygame.QUIT:
 					running = False
 
-				pygame.display.flip()
+			self.window.fill('BLACK')
 
+			self.mouse = pygame.mouse.get_pos()
+			self.dot_follow_mouse()
+
+
+			pygame.display.flip()
+
+	def dot_follow_mouse(self):
+		if self.mouse == (0,0):
+			return
+		pygame.draw.circle(self.window, 'white', self.mouse, 16)

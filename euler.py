d81259cd9373dc8278f3b6682ddf4002707732a7
Thomas <tjgriffin77@gmail.com>
2023-12-18 12:42:04 -0500

    code cleanup

--- euler.py
+++ euler.py
@@ -4,20 +4,20 @@ from typing import Tuple
 from constants import *
 
 class Vertex:
-    def __init__(self, window: Surface, position: Tuple[int, int]):
+    def __init__(self, window: Surface, position: Tuple[int, int]) -> None:
         self.window = window
         self.position = position
         self.rect = Rect
 
-    def draw(self):
+    def draw(self) -> None:
         self.rect = draw.circle(self.window, VERTEX_COLOR, self.position, VERTEX_SIZE)
 
 class Edge:
-    def __init__(self, window: Surface, vertex1: Vertex, vertex2: Vertex):
+    def __init__(self, window: Surface, vertex1: Vertex, vertex2: Vertex) -> None:
         self.window = window
         self.vertex1 = vertex1
         self.vertex2 = vertex2
         self.rect = Rect
 
-    def draw(self):
+    def draw(self) -> None:
         self.rect = draw.line(self.window, EDGE_COLOR, self.vertex1.position, self.vertex2.position, EDGE_SIZE)
--- screen.py
+++ screen.py
@@ -32,53 +32,17 @@ class Screen:
                     running = False
  
                 if event.type == pygame.MOUSEMOTION:
-                    for button in self.menu.buttons:
-                        if button.rect.left < self.mouse[0] < button.rect.right and button.rect.top < self.mouse[1] < button.rect.bottom:
-                            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
-                            button.hover()
-                            break
-                        else:
-                            button.idle()
-                            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+                    self.button_motion_loop()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # Place vertices on click
-                    if self.mouse[1] > MENU_HEIGHT and self.mode == "Vertex":
-                        self.vertices.append(Vertex(self.window, self.mouse))
-                    # Place edges on hold
-                    if self.mouse[1] > MENU_HEIGHT and self.mode == "Edge":
-                        # left click
-                        if event.button == 1:
-                            for vertex in self.vertices:
-                                if vertex.rect.collidepoint(event.pos):
-                                    if not self.selecting:
-                                        self.selecting = True
-                                        self.selection = vertex
-                                        print(self.selection)
-                                    else:
-                                        new_edge = Edge(self.window, self.selection, vertex)
-                                        for edge in self.edges:
-                                            if (new_edge.vertex1 == edge.vertex1 and new_edge.vertex2 == edge.vertex2) or (new_edge.vertex1 == edge.vertex2 and new_edge.vertex2 == edge.vertex1):
-                                                self.selecting = False
-                                        if self.selecting:
-                                            self.edges.append(new_edge)
-                                            self.selecting = False
-                                            self.selection = None
-                                        print(self.edges[-1])
-                                    
-                        # right click 
-                        if event.button == 3:
-                            self.selecting = False
-                            print(self.selecting)
+                    self.vertex_loop()
+                    self.edge_loop(event)
 
-                    # Check for button clicks
-                    for button in self.menu.buttons:
-                        if button.rect.left < self.mouse[0] < button.rect.right and button.rect.top < self.mouse[1] < button.rect.bottom:
-                            self.mode = button.press()
+                    self.button_click_loop()
 
             pygame.display.flip()
 
-    def draw(self):
+    def draw(self) -> None:
         self.window.fill(SCREEN_BACKGROUND)
             
         for vertex in self.vertices:
@@ -102,6 +66,52 @@ class Screen:
             return
         pygame.draw.circle(self.window, CURSOR_COLOR, self.mouse, VERTEX_SIZE)
 
+    def vertex_loop(self) -> None:
+        # Place vertices on click
+        if self.mouse[1] > MENU_HEIGHT and self.mode == "Vertex":
+            self.vertices.append(Vertex(self.window, self.mouse))
+
+    def edge_loop(self, event: pygame.event.Event) -> None:
+        # Place edges on hold
+        if self.mouse[1] > MENU_HEIGHT and self.mode == "Edge":
+            # left click
+            if event.button == 1:
+                for vertex in self.vertices:
+                    if vertex.rect.collidepoint(event.pos):
+                        if not self.selecting:
+                            self.selecting = True
+                            self.selection = vertex
+                        else:
+                            new_edge = Edge(self.window, self.selection, vertex)
+                            for edge in self.edges:
+                                if (new_edge.vertex1 == edge.vertex1 and new_edge.vertex2 == edge.vertex2) or (new_edge.vertex1 == edge.vertex2 and new_edge.vertex2 == edge.vertex1):
+                                    self.selecting = False
+                            if self.selecting:
+                                self.edges.append(new_edge)
+                                self.selecting = False
+                                self.selection = None
+                        
+            # right click 
+            if event.button == 3:
+                self.selecting = False
+
+    def button_click_loop(self) -> None:
+        # Check for button clicks
+        for button in self.menu.buttons:
+            if button.rect.left < self.mouse[0] < button.rect.right and button.rect.top < self.mouse[1] < button.rect.bottom:
+                self.mode = button.press()
+
+    def button_motion_loop(self) -> None:
+        for button in self.menu.buttons:
+            if button.rect.left < self.mouse[0] < button.rect.right and button.rect.top < self.mouse[1] < button.rect.bottom:
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+                button.hover()
+                break
+            else:
+                button.idle()
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+    
 
 class Button:
     def __init__(self, window: pygame.Surface,  
@@ -115,31 +125,31 @@ class Button:
         self.font = pygame.font.Font('freesansbold.ttf', 32)
         self.button_text = self.font.render(self.text, True, (0,0,0))
     
-    def draw(self):
+    def draw(self) -> None:
         pygame.draw.rect(self.window, self.color, self.rect)
 
         self.window.blit(self.button_text, self.button_text.get_rect(center = self.rect.center))
 
-    def press(self):
+    def press(self) -> None:
         self.color = PRESS_BUTTON_COLOR
         return self.text
     
-    def hover(self):
+    def hover(self) -> None:
         self.color = HOVER_BUTTON_COLOR
 
-    def idle(self):
+    def idle(self) -> None:
         self.color = BUTTON_COLOR
 
 
 class Menu:
-    def __init__(self, window: pygame.Surface):
+    def __init__(self, window: pygame.Surface) -> None:
 
         self.window = window
         self.color = MENU_COLOR
         self.buttons = self.init_buttons()
         self.rect = pygame.rect.Rect(0, 0, WIDTH, MENU_HEIGHT)
 
-    def init_buttons(self):
+    def init_buttons(self) -> list[Button]:
         res = []
 
         # Append all buttons required.
@@ -153,7 +163,7 @@ class Menu:
 
         return res
 
-    def draw(self):
+    def draw(self) -> None:
         pygame.draw.rect(self.window, self.color, self.rect)
         for button in self.buttons:
             button.draw()
